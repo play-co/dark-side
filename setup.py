803bbc92520c4f4ca9f09b33f23e8ee77ddc9e50
mark neyer <mark@neyer.me>
2012-11-13 17:12:33 -0800

    updated setup.py to make sure all libs work

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ from distutils.core import setup
 
 
 setup(name='dark-side',
-      version='1.0.1',   
+      version='1.0.5',   
       description='response comparing proxy server', 
-      requires=['gevent','requests','json_tools'],
       author='mark neyer',
+      url='https://github.com/gameclosure/dark-side',
+      install_requires=['gevent','requests','json_tools','webob'],
       scripts=['darkside.py'])
